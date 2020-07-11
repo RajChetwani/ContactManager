219bf297174b89ba3911bc7867c90de914f2fbd8
rajd12 <2016.raj.chetwani@ves.ac.in>
2020-07-11 12:46:44 +0530

    User and Contact models added with functionalities - Backend Done

--- routes/auth.js
+++ routes/auth.js
@@ -1,21 +1,72 @@
 const express = require('express');
-
+const { check , validationResult } = require('express-validator');
+const User = require('../models/Users');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const auth = require('../middleware/auth')
 const router = express.Router();
 
 //@route  GET api/auth
 //@desc   get logged in User
 //@access Private
 
-router.get('/',(req,res) => {
-    res.send(' Get Loggedin user');
+router.get('/',auth,async(req,res) => {
+    try {
+     const user = await User.findById(req.user.id).select('-password');
+     res.send(user);
+        
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Server error");
+    }
 })
 
 //@route  POST api/auth
 //@desc   Auth user and token
 //@access Public
 
-router.post('/',(req,res) => {
-    res.send('Login user');
+router.post('/',[ check('email','Please enter valid email').isEmail(),
+    check('password','Password is required').exists()],
+    async (req,res) => {
+        const errors = validationResult(req);
+        if(!errors.isEmpty()){
+            return res.status(400).json({ errors : errors.array()})
+        }
+    
+        const { name,email,password } = req.body
+    
+        try {
+    
+            let user = await User.findOne({ email });
+            if(!user){
+                return res.status(400).json({ msg : "Invalid Credentials"});
+            }
+
+            const isMatch = await bcrypt.compare(password,user.password);
+
+            if(!isMatch){
+                return res.status(400).json({ msg : 'Invalid Credentials'});
+            }
+
+            const payload = {
+                user:{
+                    id: user.id
+                }
+            }
+    
+            jwt.sign(payload,config.get('jwtSecret'),{
+                expiresIn : 360000
+            },(err,token) =>{
+                if(err) throw err;
+                res.json({ token })
+            })
+
+        }catch(error){
+            console.error(error.message);
+            return res.status(500).send('server error');
+        }
+    
 })
 
 module.exports = router;
