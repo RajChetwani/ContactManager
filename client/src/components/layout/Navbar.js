1ef4fe8170fa555fafd07737caf3ff30a520616e
rajd12 <2016.raj.chetwani@ves.ac.in>
2020-07-18 13:57:53 +0530

    Prepare to deploy

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,13 +1,18 @@
 import React, { Fragment,useContext } from 'react'
 import {Link} from 'react-router-dom';
 import AuthContext from '../../context/auth/authContext'
+import ContactContext from '../../context/contact/contactContext'
+
 
 const Navbar = () => {
     const authContext = useContext(AuthContext);
+    const contactContext = useContext(ContactContext);
+    const { clearContacts } = contactContext;
     const { isAuthenticated,logout,user} =authContext;
 
     const onLogout = () => {
         logout();
+        clearContacts();
     }
 
     const authLinks = (
